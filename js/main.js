e1c547e196daa09b11dc99efdb991011e756e79f
Allan Epifanio <allanstefanin@gmail.com>
2021-01-06 11:39:02 -0300

    Conclusão das Aulas

---
+++ js/main.js
@@ -0,0 +1,33 @@
+/*var nome = "Allan Epifanio";
+var idade = 42;
+var idade2 = 10;
+*/
+//alert(nome + " tem " + idade + " anos!");
+//alert(idade + idade2);
+/*var d = new Date();
+alert(d.getDate());*/
+/*
+var Xmas95 = new Date('December 25, 1995 23:15:30');
+var weekday = Xmas95.getDay();
+
+console.log(weekday); // 1
+
+var options = { weekday: 'long'};
+console.log(new Intl.DateTimeFormat('pt-br', options).format(Xmas95));
+*/
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+*/
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+    
+}
+
+function redirecionar(){
+    window.open("https://passarela.com/");
+    //window.location.href = "https://passarela.com/";
+}
+
